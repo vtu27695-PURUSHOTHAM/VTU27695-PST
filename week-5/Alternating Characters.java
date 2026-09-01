ee315561e9d7c4e8c0180b2d5f18381f01ed8f77
vtu27695-PURUSHOTHAM <vtu27695@veltech.edu.in>
2026-09-01 09:41:00 +0530

    Create Alternating Characters.java

---
+++ week-5/Alternating Characters.java
@@ -0,0 +1,16 @@
+class Result {
+
+    public static int alternatingCharacters(String s) {
+
+        int deletions = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+}
